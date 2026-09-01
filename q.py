2be88d3a3468bb93f36274e23ae43a82727ffd85
savkost <savkost@gmail.com>
2026-09-01 21:40:11 +0300

    deploy: v0.27.0 - Составные серии в анализе и файлах Garmin: повтор из нескольких шагов

--- q.py
+++ q.py
@@ -1,3 +1,6 @@
-﻿import sqlite3
+﻿import sqlite3, json
 con = sqlite3.connect("/opt/running-bot/running_bot.db")
-print(con.execute("SELECT id, telegram_id, username FROM users WHERE username=\'t_savkost\'").fetchone())
+r = con.execute("SELECT analyzed_json FROM workout_analysis WHERE workout_date=? AND is_valid=1 ORDER BY id DESC LIMIT 1", ("2026-09-01",)).fetchone()
+a = json.loads(r[0])
+print("groups:", [g.get("number") for g in a.get("groups", [])])
+print("extra:", a.get("extra_groups"))
